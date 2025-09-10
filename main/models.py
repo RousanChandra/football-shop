09aac09dccd7b27b71f0ed5ff306a3348b82fac0
RousanChandra <syahrousan@gmail.com>
2025-09-10 09:38:34 +0700

    update tugas 2

--- main/models.py
+++ main/models.py
@@ -3,6 +3,14 @@ import uuid
 from django.db import models
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('shoes', 'Sepatu Bola'),
+        ('baju', 'Baju Bola'),
+        ('ball', 'Bola'),
+        ('equipment', 'Peralatan Latihan'),
+        ('accessories', 'Aksesoris'),
+        ('other', 'Lainnya'),
+    ]
     name = models.CharField(max_length=255)            # name (CharField)
     price = models.IntegerField()                      # price (IntegerField)
     description = models.TextField(blank=True)         # description (TextField)
