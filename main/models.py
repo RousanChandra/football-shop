2b29fe213f57a67be1c001f35bf18cc6dae63ed0
RousanChandra <syahrousan@gmail.com>
2025-09-15 14:53:33 +0700

    tugas 3

--- main/models.py
+++ main/models.py
@@ -3,22 +3,36 @@ import uuid
 from django.db import models
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('bola sepak', 'Bola Sepak'),
+        ('bola latihan', 'Bola Latihan'),
+        ('pakaian', 'Pakaian'),
+        ('sepatu sepak bola', 'Sepatu Sepak Bola'),
+        ('aksesoris', 'Aksesoris'),
+        ('gawang', 'Gawang'),
+        ('budget', 'Budget'),
+    ]
 
     name = models.CharField(max_length=255)            # name (CharField)
     price = models.IntegerField()                      # price (IntegerField)
     description = models.TextField(blank=True)         # description (TextField)
     thumbnail = models.URLField(blank=True, null=True) # thumbnail (URLField)
-    category = models.CharField(max_length=100, blank=True)  # category (CharField)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES,blank=True)  # category (CharField)
     is_featured = models.BooleanField(default=False)   # is_featured (BooleanField)
-    
+
     
     def __str__(self):
         return self.name
     
-    @property
-    def is_news_hot(self):
-        return self.news_views > 20
+    # @property
+    # def is_news_hot(self):
+    #     return self.news_views > 20
         
-    def increment_views(self):
-        self.news_views += 1
-        self.save()
+    # def increment_views(self):
+    #     self.news_views += 1
+    #     self.save()
+
+# class Employee(models.Model):
+#     name = models.CharField(max_length=200)
+#     umur = models.IntegerField()
+#     persona = models.TextField(blank=True)
--- main/views.py
+++ main/views.py
@@ -1,18 +1,63 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from main.forms import ProductForm
+from main.models import Product
+from django.http import HttpResponse
+from django.core import serializers
 # Create your views here.
-from .models import Product
 
 
 def home(request):
+    product_list = Product.objects.all()
     context = {
         'npm' : '2406435894',
         'student_name': 'Rousan Chandra Syahbunan',
         'class': 'PBP B',
-        'product_name': 'Bola Adidas Tango',
-        'price': 350000,
-        'description': 'Bola sepak original Adidas Tango dengan kualitas tinggi, cocok untuk pertandingan maupun latihan.',
-        'category': 'Peralatan Olahraga',
-        'is_featured': True,
+        'product_list': product_list,
     }
     return render(request, 'main.html', context)
+
+def show_xml(request):
+    Product_list = Product.objects.all()
+    xml_data = serializers.serialize("xml",  Product_list)
+    return HttpResponse(xml_data, content_type="application/xml")
+
+def show_json(request):
+    Product_list =  Product.objects.all()
+    json_data = serializers.serialize("json",  Product_list)
+    return HttpResponse(json_data, content_type="application/json")
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:home')
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
+
+def show_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, "product_detail.html", context)
+
+
+def show_xml_by_id(request, product_id):
+   try:
+       news_item = Product.objects.filter(pk=product_id)
+       xml_data = serializers.serialize("xml", news_item)
+       return HttpResponse(xml_data, content_type="application/xml")
+   except Product.DoesNotExist:
+       return HttpResponse(status=404)
+
+def show_json_by_id(request, product_id):
+   try:
+       news_item = Product.objects.get(pk=product_id)
+       json_data = serializers.serialize("json", [news_item])
+       return HttpResponse(json_data, content_type="application/json")
+   except Product.DoesNotExist:
+       return HttpResponse(status=404)
